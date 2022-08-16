57f2af3e3e79a509dd65571d595c5c9f5a70c01a
FestimsRepo <102750865+FestimP@users.noreply.github.com>
2022-08-16 15:25:05 +0200

    "kc"

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
            <div className="App">
 		   <ul className='cop'>
   <li><a class="active" href="/">Home</a></li>
-  <li><a href="/about">News</a></li>
+  <li><a href="/components/about/SignInOut">News</a></li>
   <li><a href="/Contact">Contact</a></li>
-  <li><a href="/about">Sign In/Sign up</a></li>
+  <li><a href="/components/about/SignInOut">Sign In/Sign up</a></li>
 </ul>
            <Routes>
                  <Route exact path='/' element={<Nav />}></Route>
-                 <Route exact path='/about' element={<Appes />}></Route>
+                 <Route exact path='/components/about/SignInOut' element={<Appes />}></Route>
                  <Route exact path='/Contact' element={< Contact />}></Route>
           </Routes>
           </div>
